d028fea5f4d75a571cddf79dc095cab3ee86b327
jackielin97 <tun52511@temple.edu>
2024-11-18 02:42:42 -0500

    Added Dashboard

--- HomeEstate/Controllers/HomeController.cs
+++ HomeEstate/Controllers/HomeController.cs
@@ -1,23 +1,61 @@
 using HomeEstate.Models;
+using HomeEstate.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using RealEstate.Models;
+using System.Data;
 using System.Diagnostics;
+
 
 namespace HomeEstate.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        /* private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+         public HomeController(ILogger<HomeController> logger)
+         {
+             _logger = logger;
+         }*/
+        public IActionResult Dashboard()
         {
-            _logger = logger;
+            DBConnect objDB = new DBConnect();
+            DataSet ds = objDB.GetDataSet(@"SELECT Address_Number, Address_Name, AddressCity, AddressState, AddressZip, 
+                                           Property_Type, Heating, Cooling, Year_Build, Garage, 
+                                           Utilities, Description, AskingPrice, Status 
+                                    FROM Home");
+
+            List<Home> homes = new List<Home>();
+
+            foreach (DataRow record in ds.Tables[0].Rows)
+            {
+                var home = new Home
+                {
+                    Address_Number = record["Address_Number"].ToString(),
+                    Address_Name = record["Address_Name"].ToString(),
+                    AddressCity = record["AddressCity"].ToString(),
+                    AddressState = record["AddressState"].ToString(),
+                    AddressZip = record["AddressZip"].ToString(),
+                    Property_Type = record["Property_Type"].ToString(),
+                     Heating = record["Heating"].ToString(),
+                    Cooling = record["Cooling"].ToString(),
+                    Year_Build = Convert.ToInt32(record["Year_Build"].ToString()),
+                    Garage = record["Garage"].ToString(),
+                    Utilities = record["Utilities"].ToString(),
+                    Description = record["Description"].ToString(),
+                    AskingPrice = Convert.ToInt32(record["AskingPrice"].ToString()),
+                    Status = record["Status"].ToString()
+                };
+
+                homes.Add(home);
+            }
+
+            ViewBag.HomeList = homes;  // Using ViewBag to pass data to the view
+
+            return View("Dashboard");
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+
 
         public IActionResult Privacy()
         {
---
+++ HomeEstate/Models/ResetPasswordModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeEstate.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public  String UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
