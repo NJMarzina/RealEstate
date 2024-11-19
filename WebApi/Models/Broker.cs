7841b513f9b45061d499024b7f3dd21b65e43c88
jackielin97 <tun52511@temple.edu>
2024-11-19 13:50:10 -0500

    Models Folder

---
+++ WebApi/Models/Broker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+
+namespace WebApi.Models
+{
+    public class Broker
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+
+        public Broker()
+        {
+
+        }
+
+        public Broker(String username, String password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+    }
+}
