69f6c543878d05b43678ad980d4f0308aaf90bfd
jackielin97 <tun52511@temple.edu>
2024-12-06 16:45:44 -0500

    3rd party web api

--- HomeLibrary/obj/Debug/net8.0/HomeLibrary.AssemblyInfo.cs
+++ HomeLibrary/obj/Debug/net8.0/HomeLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48fb2faec99f0e9f871b80322d408c297a30ef52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babc15add6e060e59b4da0680d2acb8f3a72da47")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
